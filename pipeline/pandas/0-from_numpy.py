2783250b533150b733b06b4fb183c2da264f28a5
DevAyyub <ayyub.mammadov.2005@gmail.com>
2026-03-02 11:07:20 +0000

    Task 0: from numpy

---
+++ pipeline/pandas/0-from_numpy.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import pandas as pd
+
+
+def from_numpy(array):
+
+    num_cols = array.shape[1]
+
+    column_labels = [chr(65 + i) for i in range(num_cols)]
+
+    return pd.DataFrame(array, columns=column_labels)
