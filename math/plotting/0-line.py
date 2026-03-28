b5e9fae7e8ed2b8a69d32dea2fe483bcb3294c0d
DevAyyub <ayyub.mammadov.2005@gmail.com>
2026-03-28 22:21:49 +0000

    Task 0: Add README and plot line graph

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
+"""
+This module provides a function to plot a line graph using matplotlib.
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def line():
+    """
+    Plots y = x^3 as a solid red line with x ranging from 0 to 10.
+    """
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
