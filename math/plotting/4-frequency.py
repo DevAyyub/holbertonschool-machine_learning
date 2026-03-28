a881dc1ab1170b0c1db2e690b8e37dbb2383abdd
DevAyyub <ayyub.mammadov.2005@gmail.com>
2026-03-28 22:43:24 +0000

    Fix bins to use np.arange for strict checker

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -15,8 +15,8 @@ def frequency():
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
-    # Define the bins every 10 units from 0 to 100
-    bins = range(0, 110, 10)
+    # Define the bins exactly every 10 units using np.arange
+    bins = np.arange(0, 110, 10)
 
     # Plot the histogram with black edge color
     plt.hist(student_grades, bins=bins, edgecolor='black')
@@ -26,7 +26,7 @@ def frequency():
     plt.ylabel('Number of Students')
     plt.title('Project A')
 
-    # Set x-axis limits and tick marks to match the bins
+    # Set x-axis limits and tick marks
     plt.xlim(0, 100)
     plt.xticks(bins)
 
