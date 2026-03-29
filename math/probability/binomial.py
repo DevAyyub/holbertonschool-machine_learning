087a6ad6849aec28caff6420c4d6565e92527263
DevAyyub <ayyub.mammadov.2005@gmail.com>
2026-03-29 02:57:32 +0000

    Add CDF method to Binomial class

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -56,3 +56,17 @@ class Binomial:
         pmf = comb * (p ** k) * ((1 - p) ** (n - k))
 
         return pmf
+
+    def cdf(self, k):
+        """
+        Calculates the value of the CDF for a given number of 'successes'.
+        """
+        if k < 0:
+            return 0
+        k = int(k)
+
+        cdf = 0
+        for i in range(k + 1):
+            cdf += self.pmf(i)
+
+        return cdf
