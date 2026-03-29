e239ba03549aaa2f581648220c7420607e78e297
DevAyyub <ayyub.mammadov.2005@gmail.com>
2026-03-29 12:17:23 +0000

    Add Task 13: Neural Network Gradient Descent

---
+++ supervised_learning/classification/13-neural_network.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+"""
+Module defining a neural network with one hidden layer.
+"""
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """
+    Defines a neural network with one hidden layer performing
+    binary classification.
+    """
+
+    def __init__(self, nx, nodes):
+        """Initializes the neural network."""
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """Retrieves the weights vector of the hidden layer."""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """Retrieves the bias of the hidden layer."""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """Retrieves the activated output of the hidden layer."""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """Retrieves the weights vector of the output neuron."""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """Retrieves the bias of the output neuron."""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """Retrieves the activated output of the output neuron."""
+        return self.__A2
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neural network."""
+        Z1 = np.matmul(self.__W1, X) + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-Z1))
+
+        Z2 = np.matmul(self.__W2, self.__A1) + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-Z2))
+
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+        """Calculates the cost of the model using logistic regression."""
+        m = Y.shape[1]
+        loss_1 = Y * np.log(A)
+        loss_2 = (1 - Y) * np.log(1.0000001 - A)
+        cost = -(1 / m) * np.sum(loss_1 + loss_2)
+        return cost
+
+    def evaluate(self, X, Y):
+        """Evaluates the neural network predictions."""
+        A1, A2 = self.forward_prop(X)
+        cost = self.cost(Y, A2)
+        prediction = np.where(A2 >= 0.5, 1, 0)
+        return prediction, cost
+
+    def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
+        """Calculates one pass of gradient descent on the neural network."""
+        m = Y.shape[1]
+
+        # Output layer error and gradients
+        dZ2 = A2 - Y
+        dW2 = np.matmul(dZ2, A1.T) / m
+        db2 = np.sum(dZ2, axis=1, keepdims=True) / m
+
+        # Hidden layer error and gradients
+        dZ1 = np.matmul(self.__W2.T, dZ2) * (A1 * (1 - A1))
+        dW1 = np.matmul(dZ1, X.T) / m
+        db1 = np.sum(dZ1, axis=1, keepdims=True) / m
+
+        # Update attributes
+        self.__W1 = self.__W1 - (alpha * dW1)
+        self.__b1 = self.__b1 - (alpha * db1)
+        self.__W2 = self.__W2 - (alpha * dW2)
+        self.__b2 = self.__b2 - (alpha * db2)
