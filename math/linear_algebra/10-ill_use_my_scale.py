26d85147a68b0c0417204c9d8de48ba095ee375b
DevAyyub <ayyub.mammadov.2005@gmail.com>
2026-03-29 00:32:22 +0000

    Task 10: I'll use my scale

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module to get the shape of a numpy array"""
+
+
+def np_shape(matrix):
+    """Calculates the shape of a numpy.ndarray
+    Args:
+        matrix: The numpy.ndarray to calculate the shape of
+    Returns:
+        A tuple of integers representing the shape
+    """
+    return matrix.shape
