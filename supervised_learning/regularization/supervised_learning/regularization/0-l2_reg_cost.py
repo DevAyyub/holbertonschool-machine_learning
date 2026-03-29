31f8cb6bd93c0576f2311bf8edfa9c8fd1ef1c7b
DevAyyub <ayyub.mammadov.2005@gmail.com>
2026-03-29 14:10:16 +0000

    Fix pycodestyle W293 and implement l2_reg_cost

---
+++ supervised_learning/regularization/supervised_learning/regularization/0-l2_reg_cost.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Module to calculate L2 Regularization Cost
+"""
+import numpy as np
+
+
+def l2_reg_cost(cost, lambtha, weights, L, m):
+    """
+    Calculates the cost of a neural network with L2 regularization
+    Args:
+        cost: cost of the network without L2 regularization
+        lambtha: regularization parameter
+        weights: dictionary of weights and biases (numpy.ndarrays)
+        L: number of layers in the neural network
+        m: number of data points used
+    Returns: the cost of the network accounting for L2 regularization
+    """
+    l2_cost_total = 0
+    for i in range(1, L + 1):
+        key = "W{}".format(i)
+        l2_cost_total += np.sum(np.square(weights[key]))
+
+    l2_reg = (lambtha / (2 * m)) * l2_cost_total
+    return cost + l2_reg
