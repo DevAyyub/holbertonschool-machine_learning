a46060ef3dc8a4ed1861df487cc9a6a44cdc1e0e
DevAyyub <ayyub.mammadov.2005@gmail.com>
2026-03-29 00:32:57 +0000

    Task 11: The Western Exchange

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module to transpose a numpy array"""
+
+
+def np_transpose(matrix):
+    """Transposes a numpy.ndarray
+    Args:
+        matrix: The numpy.ndarray to transpose
+    Returns:
+        A new numpy.ndarray (transposed)
+    """
+    return matrix.transpose()
