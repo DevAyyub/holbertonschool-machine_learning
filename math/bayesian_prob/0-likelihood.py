4a82d0c4137ea6080b02a99ae1f30966eeb1af6f
DevAyyub <ayyub.mammadov.2005@gmail.com>
2026-03-29 03:05:43 +0000

    Add likelihood function for Bayesian Probability

---
+++ math/bayesian_prob/0-likelihood.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Module to calculate the likelihood of patient side effects.
+"""
+import numpy as np
+
+
+def likelihood(x, n, P):
+    """
+    Calculates the likelihood of obtaining data given various
+    hypothetical probabilities of developing severe side effects.
+    """
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x < 0:
+        message = "x must be an integer that is greater than or equal to 0"
+        raise ValueError(message)
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if not np.all((P >= 0) & (P <= 1)):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+    import math
+    fact_n = math.factorial(n)
+    fact_x = math.factorial(x)
+    fact_nx = math.factorial(n - x)
+    combination = fact_n / (fact_x * fact_nx)
+
+    return combination * (P ** x) * ((1 - P) ** (n - x))
