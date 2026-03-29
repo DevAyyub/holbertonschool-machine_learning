8e2c84ffb729a79da05b14bc18a15a68ed03a198
DevAyyub <ayyub.mammadov.2005@gmail.com>
2026-03-29 02:54:13 +0000

    Add CDF method to Normal class using erf approximation

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -53,3 +53,17 @@ class Normal:
         coefficient = 1 / (self.stddev * ((2 * pi) ** 0.5))
 
         return coefficient * (e ** exponent)
+
+    def cdf(self, x):
+        """
+        Calculates the value of the CDF for a given x-value.
+        """
+        pi = 3.1415926536
+        val = (x - self.mean) / (self.stddev * (2 ** 0.5))
+        
+        erf = (2 / (pi ** 0.5)) * (
+            val - (val ** 3) / 3 + (val ** 5) / 10 -
+            (val ** 7) / 42 + (val ** 9) / 216
+        )
+        
+        return 0.5 * (1 + erf)
