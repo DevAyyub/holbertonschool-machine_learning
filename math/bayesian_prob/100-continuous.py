deef1f6af324a61f037d48b11e26aba03e7e0df3
DevAyyub <ayyub.mammadov.2005@gmail.com>
2026-03-29 03:16:33 +0000

    Add continuous posterior probability function

---
+++ math/bayesian_prob/100-continuous.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Module to calculate the continuous posterior probability.
+"""
+from scipy import special
+
+
+def posterior(x, n, p1, p2):
+    """
+    Calculates the posterior probability that the probability of developing
+    severe side effects falls within a specific range [p1, p2].
+    """
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x < 0:
+        msg = "x must be an integer that is greater than or equal to 0"
+        raise ValueError(msg)
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+
+    if not isinstance(p1, float) or not (0 <= p1 <= 1):
+        raise ValueError("p1 must be a float in the range [0, 1]")
+    if not isinstance(p2, float) or not (0 <= p2 <= 1):
+        raise ValueError("p2 must be a float in the range [0, 1]")
+
+    if p2 <= p1:
+        raise ValueError("p2 must be greater than p1")
+
+    a = x + 1
+    b = n - x + 1
+
+    return special.betainc(a, b, p2) - special.betainc(a, b, p1)
