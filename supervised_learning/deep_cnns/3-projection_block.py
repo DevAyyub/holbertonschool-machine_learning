295999f62b408f65e61e37e3e455a7e4340b8351
DevAyyub <ayyub.mammadov.2005@gmail.com>
2026-04-19 19:50:35 +0000

    Fix pycodestyle line length in 3-projection_block

--- supervised_learning/deep_cnns/3-projection_block.py
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -14,19 +14,33 @@ def projection_block(A_prev, filters, s=2):
     init = K.initializers.HeNormal(seed=0)
 
     # Main Path
-    X = K.layers.Conv2D(filters=F11, kernel_size=(1, 1), strides=(s, s), padding='same', kernel_initializer=init)(A_prev)
+    X = K.layers.Conv2D(filters=F11,
+                        kernel_size=(1, 1),
+                        strides=(s, s),
+                        padding='same',
+                        kernel_initializer=init)(A_prev)
     X = K.layers.BatchNormalization(axis=3)(X)
     X = K.layers.Activation('relu')(X)
 
-    X = K.layers.Conv2D(filters=F3, kernel_size=(3, 3), padding='same', kernel_initializer=init)(X)
+    X = K.layers.Conv2D(filters=F3,
+                        kernel_size=(3, 3),
+                        padding='same',
+                        kernel_initializer=init)(X)
     X = K.layers.BatchNormalization(axis=3)(X)
     X = K.layers.Activation('relu')(X)
 
-    X = K.layers.Conv2D(filters=F12, kernel_size=(1, 1), padding='same', kernel_initializer=init)(X)
+    X = K.layers.Conv2D(filters=F12,
+                        kernel_size=(1, 1),
+                        padding='same',
+                        kernel_initializer=init)(X)
     X = K.layers.BatchNormalization(axis=3)(X)
 
     # Shortcut Path
-    X_shortcut = K.layers.Conv2D(filters=F12, kernel_size=(1, 1), strides=(s, s), padding='same', kernel_initializer=init)(A_prev)
+    X_shortcut = K.layers.Conv2D(filters=F12,
+                                 kernel_size=(1, 1),
+                                 strides=(s, s),
+                                 padding='same',
+                                 kernel_initializer=init)(A_prev)
     X_shortcut = K.layers.BatchNormalization(axis=3)(X_shortcut)
 
     # Add shortcut value to main path
